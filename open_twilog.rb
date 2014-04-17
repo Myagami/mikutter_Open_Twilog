f00e1154571bbe8cb87972c6e9255e384eff6944
M.Yagami <hayate@Reinforce-Eins.(none)>
2014-04-17 17:53:43 +0900

    event rename

--- open_twilog.rb
+++ open_twilog.rb
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-
 # favstarを開く
 
-Plugin.create(:open_favstar) do
-  command(:open_favstar,
-          name: 'こいつのfavstarを見る',
+Plugin.create(:open_twilog) do
+  command(:open_twilog,
+          name: 'こいつのtwilogを見る',
           condition: Plugin::Command[:HasMessage],
           visible: true,
           role: :timeline) do |m|
     m.messages.map do |msg|
-      Gtk::openurl("http://twilor.org/#{msg.message.user.idname}")
+      Gtk::openurl("http://twilog.org/#{msg.message.user.idname}")
     end
   end
 end
